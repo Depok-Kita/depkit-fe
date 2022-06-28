27c1dcf867094ae220909ddac85f3bd00cb38320
edutjie <edutjitrahardja@gmail.com>
2022-06-28 22:32:49 +0700

    Responsive v1

--- pages/comingsoon.tsx
+++ pages/comingsoon.tsx
@@ -3,23 +3,31 @@ import React from "react";
 import Image from "next/image";
 import { Header, Button } from "@components";
 import Link from "next/link";
+import { useWindowSize } from "@hooks";
 
 const ComingSoon: NextPage = () => {
+  const { width } = useWindowSize();
   return (
     <div className="bg-powder-light min-h-screen flex justify-center items-center">
-      <div className="flex flex-col max-w-[500px] justify-center gap-6">
+      <div className="flex flex-col max-w-[500px] justify-center gap-3 tablet:gap-6">
         <Image
           src="/assets/images/coming-soon-asset.svg"
           alt="Coming Soon"
-          width={500}
-          height={222}
+          width={width >= 768 ? 500 : 260}
+          height={width >= 768 ? 222 : 115}
         />
-        <div className="flex flex-col gap-[66px]">
-          <Header preset="decorative" className="text-center">
+        <div className="flex flex-col gap-8 tablet:gap-10 desktop:gap-[66px]">
+          <Header
+            preset="decorative"
+            className="text-center tablet:scale-100 tablet:text-[52px] desktop:text-[64px]"
+          >
             Coming Soon!
           </Header>
           <Link href={"/"} passHref>
-            <Button preset="primaryDark" className="mx-auto scale-150">
+            <Button
+              preset="primaryDark"
+              className="mx-auto scale-100 tablet:scale-150"
+            >
               Kembali
             </Button>
           </Link>
