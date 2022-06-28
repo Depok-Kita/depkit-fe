a96081d01e65b5cd3b63acf12d042b2c5ede5927
Naufal W <naufalwwidyatama@gmail.com>
2022-06-28 21:39:52 +0700

    Add decoration

--- pages/404.tsx
+++ pages/404.tsx
@@ -1,24 +1,41 @@
 import { Body, Button, Header } from "@components";
 import { useRouter } from "next/router";
+import Cone from "@images/cone.png";
+import Image from "next/image";
 
 export default function Custom404() {
   const router = useRouter();
   return (
-    <div className="bg-powder-light px-10 py-5 min-h-screen">
-      <Header preset="h3" className="hidden md:block pb-20">
-        404 Not Found
-      </Header>
-      <Header preset="h2" className="text-cerulean">
-        404
-      </Header>
-      <Body preset="p1">Maaf, halaman yang kamu cari tidak ada.</Body>
-      <Button
-        preset="primaryDark"
-        onClick={() => router.back()}
-        className="mt-10"
-      >
-        Kembali
-      </Button>
+    <div className="bg-powder-light px-10 py-5 min-h-screen flex justify-center">
+      <div className="px-[10%]">
+        <Header preset="h3" className="hidden md:block">
+          404 Not Found
+        </Header>
+        <div className="lg:grid grid-cols-2 justify-center content-center">
+          <div className="text-center lg:text-left">
+            <Header preset="h2" className="text-cerulean md:mt-20">
+              404
+            </Header>
+            <Body preset="p1">Maaf, halaman yang kamu cari tidak ada.</Body>
+            <div className="lg:hidden">
+              <Image src={Cone} className="lg:hidden" />
+            </div>
+
+            <div className="flex justify-center lg:justify-start">
+              <Button
+                preset="primaryDark"
+                onClick={() => router.back()}
+                className="lg:mt-10"
+              >
+                Kembali
+              </Button>
+            </div>
+          </div>
+          <div className="hidden lg:flex justify-end">
+            <Image src={Cone} className="" />
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
