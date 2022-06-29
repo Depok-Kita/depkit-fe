d47359c36d9a1f7d125eaf3bc69d69f39f46024e
Naufal W <naufalwwidyatama@gmail.com>
2022-06-29 14:47:41 +0700

    Replace screen names

--- pages/404.tsx
+++ pages/404.tsx
@@ -8,30 +8,30 @@ export default function Custom404() {
   return (
     <div className="bg-powder-light px-10 py-5 min-h-screen flex justify-center">
       <div className="px-[10%]">
-        <Header preset="h3" className="hidden md:block">
+        <Header preset="h3" className="hidden tablet:block">
           404 Not Found
         </Header>
-        <div className="lg:grid grid-cols-2 justify-center content-center">
-          <div className="text-center lg:text-left">
-            <Header preset="h2" className="text-cerulean md:mt-20">
+        <div className="desktop:grid grid-cols-2 justify-center content-center">
+          <div className="text-center desktop:text-left">
+            <Header preset="h2" className="text-cerulean tablet:mt-20">
               404
             </Header>
             <Body preset="p1">Maaf, halaman yang kamu cari tidak ada.</Body>
-            <div className="lg:hidden">
-              <Image src={Cone} className="lg:hidden" />
+            <div className="desktop:hidden">
+              <Image src={Cone} className="desktop:hidden" />
             </div>
 
-            <div className="flex justify-center lg:justify-start">
+            <div className="flex justify-center desktop:justify-start">
               <Button
                 preset="primaryDark"
                 onClick={() => router.back()}
-                className="lg:mt-10"
+                className="desktop:mt-10"
               >
                 Kembali
               </Button>
             </div>
           </div>
-          <div className="hidden lg:flex justify-end">
+          <div className="hidden desktop:flex justify-end">
             <Image src={Cone} className="" />
           </div>
         </div>
