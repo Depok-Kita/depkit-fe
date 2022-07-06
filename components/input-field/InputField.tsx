2528da827d76cabc25eb908bca04539100446621
Naufal Weise <naufalwwidyatama@gmail.com>
2022-07-06 21:15:49 +0700

    Component - Input Field (#50)
    * Add inputfield
    
    * Fix field icon styling when disabled
    
    * add selectfield
    
    * Remove unused imports
    
    * Add comments describing props
    
    * Add font
    
    Co-authored-by: marcellinuselbert11 <marcellinuselbert46@gmail.com>

---
+++ components/input-field/InputField.tsx
@@ -0,0 +1,79 @@
+import {
+  FormControl,
+  FormErrorMessage,
+  Input,
+  InputGroup,
+  InputLeftElement,
+  InputRightElement,
+} from "@chakra-ui/react";
+import { FormLabel } from "./FormLabel";
+// field contains label & input element
+type InputFieldProps = {
+  className?: string; // className of field
+
+  type: "text" | "number" | "password" | "email"; // input type
+  name?: string; // field name, ex. firstname, password
+  label?: string;
+
+  value?: string | number; // input value
+  onChange?: React.ChangeEventHandler<HTMLInputElement>;
+
+  placeholder?: string;
+  errorMessage?: string;
+
+  isDisabled?: boolean;
+  isError?: boolean;
+
+  leftIcon?: React.ReactNode;
+  rightIcon?: React.ReactNode;
+};
+
+export const InputField = (props: InputFieldProps) => {
+  return (
+    <FormControl isInvalid={props.isError} className={props.className}>
+      <FormLabel htmlFor={props.name}>{props.label}</FormLabel>
+      <InputGroup>
+        {props.leftIcon && (
+          <InputLeftElement
+            pointerEvents="none"
+            children={
+              <Icon isDisabled={props.isDisabled}>{props.leftIcon}</Icon>
+            }
+          />
+        )}
+        <Input
+          type={props.type}
+          name={props.name}
+          value={props.value}
+          placeholder={props.placeholder}
+          onChange={props.onChange}
+          isDisabled={props.isDisabled}
+          className="font-jakarta-sans hover:border-cerulean"
+        />
+        {props.rightIcon && (
+          <InputRightElement
+            pointerEvents="none"
+            children={
+              <Icon isDisabled={props.isDisabled}>{props.rightIcon}</Icon>
+            }
+          />
+        )}
+      </InputGroup>
+
+      {props.isError && (
+        <FormErrorMessage>{props.errorMessage}</FormErrorMessage>
+      )}
+    </FormControl>
+  );
+};
+
+type IconPropsType = {
+  isDisabled?: boolean;
+  children: React.ReactNode;
+};
+
+function Icon(props: IconPropsType) {
+  return (
+    <div className={props.isDisabled ? "opacity-40" : ""}>{props.children}</div>
+  );
+}
