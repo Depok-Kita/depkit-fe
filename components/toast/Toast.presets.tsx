520fc64a13db85cc776fc2323b062075fc932572
edutjie <edutjitrahardja@gmail.com>
2022-07-06 18:10:34 +0700

    Upgraded Toast

--- components/toast/Toast.presets.tsx
+++ components/toast/Toast.presets.tsx
@@ -12,8 +12,6 @@ type ToastStyleProps = {
   border: string;
   borderWidth: string;
   borderColor: string;
-  hover: string;
-  disabled: string;
   image: React.SVGAttributes<SVGElement>;
 };
 
@@ -24,61 +22,34 @@ type ToastPresetProps = {
 export const TOAST_PRESETS: ToastPresetProps = {
   error: {
     border: "rounded-3xl",
-    borderWidth: "border-2  ",
-    color: "bg-danger-dark active:bg-danger-light bg-opacity-20",
-    hover: "hover:shadow-xl hover:shadow-danger-dark",
+    borderWidth: "border-2",
+    color: "bg-danger-dark/20 active:bg-danger-light backdrop-blur-sm",
     text: "text-danger-dark",
     borderColor: "border-danger-dark active:border-danger-dark",
-    disabled:
-      "disabled:bg-grey-soft  disabled:border-grey-soft disabled:text-grey disabled:hover:shadow-none",
     image: <ExclamationIcon className="w-6 h-6" />,
   },
   warning: {
     border: "rounded-3xl",
     borderWidth: "border-2",
-    color: "bg-warning-dark active:bg-warning-light bg-opacity-20",
-    hover: "hover:shadow-xl hover:shadow-warning-dark",
+    color: "bg-warning-dark/20 active:bg-warning-light backdrop-blur-sm",
     text: "text-warning-dark",
     borderColor: "border-warning-dark active:border-warning-dark",
-    disabled:
-      "disabled:bg-grey-soft  disabled:border-grey-soft disabled:text-grey disabled:hover:shadow-none",
     image: <ExclamationCircleIcon className="w-6 h-6" />,
   },
   success: {
     border: "rounded-3xl",
     borderWidth: "border-2",
-    color: "bg-success-dark active:bg-success-dark bg-opacity-20",
-    hover: "hover:shadow-xl hover:shadow-success-dark",
+    color: "bg-success-dark/20 active:bg-success-dark backdrop-blur-sm",
     text: "text-success-dark",
     borderColor: "border-success-dark active:border-success-dark",
-    disabled: "disabled:border-grey disabled:text-grey disabled:bg-denim-dark",
     image: <CheckCircleIcon className="w-6 h-6" />,
   },
   info: {
     border: "rounded-3xl",
     borderWidth: "border-2",
-    color: "bg-cerulean active:bg-cerulean bg-opacity-20",
-    hover: "hover:shadow-xl hover:shadow-cerulean",
+    color: "bg-cerulean/20 active:bg-cerulean backdrop-blur-sm",
     text: "text-cerulean",
     borderColor: "border-cerulean active:border-cerulean",
-    disabled: "disabled:border-grey disabled:text-grey disabled:bg-totalwhite",
     image: <InformationCircleIcon className="w-6 h-6" />,
-  },
-};
-
-type ToastToggleProps = {
-  visibility: string;
-};
-
-type ToastActiveProps = {
-  [key: string]: ToastToggleProps;
-};
-
-export const TOAST_TOGGLE: ToastActiveProps = {
-  on: {
-    visibility: "visible",
-  },
-  off: {
-    visibility: "invisible",
   },
 };
--- components/toast/Toast.tsx
+++ components/toast/Toast.tsx
@@ -1,64 +1,40 @@
 import React from "react";
-import { TOAST_PRESETS, TOAST_TOGGLE } from "./Toast.presets";
+import { TOAST_PRESETS } from "./Toast.presets";
+import { Box, useToast } from "@chakra-ui/react";
 import { Header } from "@components";
 import { XIcon } from "@heroicons/react/outline";
-import { Box } from "@chakra-ui/react";
 
-type ToastProps = {
-  className?: string;
-
-  to?: string;
-
-  active: "on" | "off";
-
-  padding?: string;
-  border?: string;
-
+export type ToastProps = {
   preset: "error" | "warning" | "success" | "info";
-
-  children?: React.ReactNode;
-  [props: string]: any;
+  message: React.ReactNode;
 };
 
-export const Toast = ({
-  children,
-  className,
-  preset,
-  active,
-  padding,
-  border,
-  leftIcon,
-  rightIcon,
-  ...props
-}: ToastProps) => {
-  return (
-    // using div (soon)
-    <Box
-      w="50%"
-      className={`transition-all flex justify-between items-center gap-2
-      ${TOAST_PRESETS[preset].text} md:text-base text-sm font-semibold 
-      ${TOAST_PRESETS[preset].border} 
-      ${border ? border : TOAST_PRESETS[preset].borderWidth} 
-      ${border ? border : TOAST_PRESETS[preset].borderColor} 
-      ${TOAST_PRESETS[preset].hover} 
-      ${TOAST_PRESETS[preset].color} 
-      ${padding ? padding : "py-3 px-5"} 
-      ${TOAST_TOGGLE[active].visibility}
-      ${className}`}
-      {...props}
-    >
-      <>
-        {/* ICON */}
-        {TOAST_PRESETS[preset].image}
+export const Toast = ({ preset, message }: ToastProps) => {
+  const toast = useToast({
+    position: "bottom",
+    duration: 6000,
+    render: ({ onClose }) => {
+      return (
+        <Box
+          className={`
+          transition-all flex justify-between items-center gap-2 py-3 px-5
+          ${TOAST_PRESETS[preset].text} md:text-base text-sm font-semibold 
+          ${TOAST_PRESETS[preset].border} 
+          ${TOAST_PRESETS[preset].borderWidth} 
+          ${TOAST_PRESETS[preset].borderColor}
+          ${TOAST_PRESETS[preset].color}`}
+        >
+          <>
+            {TOAST_PRESETS[preset].image}
+            <Header preset="h6">{message}</Header>
+            <button onClick={onClose}>
+              <XIcon className="h-6 w-6" />
+            </button>
+          </>
+        </Box>
+      );
+    },
+  });
 
-        <Header preset="h6">{children}</Header>
-
-        {/* CLOSE */}
-        {/* CHANGE ON TO OFF */}
-        <button onClick={() => TOAST_TOGGLE["off"]}>
-          <XIcon className="h-6 w-6" />
-        </button>
-      </>
-    </Box>
-  );
+  return toast;
 };
