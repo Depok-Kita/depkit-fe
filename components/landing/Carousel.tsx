7b552647e3813b6ccff46b1e9c5a29b1261160d6
Naufal Weise <naufalwwidyatama@gmail.com>
2022-07-10 12:41:06 +0700

    Init carousel

--- components/landing/Carousel.tsx
+++ components/landing/Carousel.tsx
@@ -1,3 +1,32 @@
+// Import Swiper React components
+import { Swiper, SwiperSlide } from "swiper/react";
+import { Autoplay } from "swiper";
+// Import Swiper styles
+import "swiper/css";
+import { HOME_CAROUSEL_IMAGES } from "@constants";
+import Image from "next/image";
+
 export const Carousel = () => {
-  return <div>Carousel</div>;
+  return (
+    <div>
+      <Swiper
+        slidesPerView={1}
+        centeredSlides={true}
+        loop={true}
+        autoplay={{
+          delay: 2500,
+          disableOnInteraction: false,
+        }}
+        onSlideChange={() => console.log("slide change")}
+        onSwiper={(swiper) => console.log(swiper)}
+        modules={[Autoplay]}
+      >
+        {HOME_CAROUSEL_IMAGES.map((link) => (
+          <SwiperSlide key={link}>
+            <Image src={link} height="700" width="800" />
+          </SwiperSlide>
+        ))}
+      </Swiper>
+    </div>
+  );
 };
---
+++ constants/home-carousel-images.ts
@@ -0,0 +1,5 @@
+export const HOME_CAROUSEL_IMAGES: string[] = [
+  "/assets/images/home-img.png",
+  "/assets/images/home-img2.png",
+  "/assets/images/home-img3.png",
+];
