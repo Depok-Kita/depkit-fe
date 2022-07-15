9facce1d2032b32abcec604402ea5f017937beb1
eugeniusms <eugeniusms@gmail.com>
2022-07-16 04:42:37 +0700

    Add mailto and wa.me

--- components/sponsor/SponsorContactPerson.tsx
+++ components/sponsor/SponsorContactPerson.tsx
@@ -14,32 +14,40 @@ export const SponsorContactPerson = () => {
           </Body>
         </div>
         <div className="flex justify-start mobile:pl-2 tablet:pl-20">
-          <button className="mobile:w-[250px] tablet:w-[490px] absolute border-none">
-            <div className="flex gap-2 items-center">
-              <ChatIcon className="mobile:w-7" />
-              <Body
-                preset="b1"
-                className="mobile:text-[14px] tablet:text-[24px]"
-              >
-                rasyid doe
-              </Body>
-            </div>
-          </button>
+          {/* https//wa.me/{phone number} */}
+          <a href="https://wa.me/">
+            <button className="mobile:w-[250px] tablet:w-[490px] absolute border-none">
+              <div className="flex gap-2 items-center">
+                <ChatIcon className="mobile:w-7" />
+                <Body
+                  preset="b1"
+                  className="mobile:text-[14px] tablet:text-[24px]"
+                >
+                  eugenius mario
+                </Body>
+              </div>
+            </button>
+          </a>
         </div>
         <br />
         <br />
         <div className="flex justify-start mobile:pl-2 tablet:pl-20">
-          <button className="mobile:w-[250px] tablet:w-[490px] absolute border-none">
-            <div className="flex gap-2 items-center">
-              <MailIcon className="mobile:w-7" />
-              <Body
-                preset="b1"
-                className="mobile:text-[14px] tablet:text-[24px]"
-              >
-                rasyiddoe@gmail.com
-              </Body>
-            </div>
-          </button>
+          <a href="mailto:eugeniusms@gmail.com?subject=[ Sponsor and Media Partner Depok Kita ]">
+            <button
+              className="mobile:w-[250px] tablet:w-[490px] absolute border-none"
+              type="submit"
+            >
+              <div className="flex gap-2 items-center">
+                <MailIcon className="mobile:w-7" />
+                <Body
+                  preset="b1"
+                  className="mobile:text-[14px] tablet:text-[24px]"
+                >
+                  eugeniusms@gmail.com
+                </Body>
+              </div>
+            </button>
+          </a>
         </div>
       </div>
     </div>
