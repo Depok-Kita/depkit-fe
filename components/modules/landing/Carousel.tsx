daf60348e04698219624aad5526f9129d0e26f9b
Naufal Weise <naufalwwidyatama@gmail.com>
2022-07-20 12:20:15 +0700

    Fix carousel sizing and add intro

--- components/modules/landing/Carousel.tsx
+++ components/modules/landing/Carousel.tsx
@@ -4,10 +4,13 @@ import { Autoplay } from "swiper";
 // Import Swiper styles
 import { HOME_CAROUSEL_IMAGES } from "@constants";
 import Image from "next/image";
+import { Body, Header } from "@components/typography";
+import { Button } from "@components/button";
+import { ChevronRightIcon } from "@heroicons/react/solid";
 
 export const Carousel = () => {
   return (
-    <div>
+    <div className="relative">
       <Swiper
         slidesPerView={1}
         centeredSlides={true}
@@ -28,6 +31,22 @@ export const Carousel = () => {
           </SwiperSlide>
         ))}
       </Swiper>
+      <div className="absolute z-10  bottom-0  right-0  left-0 w-[80%] min-h-[50%] h-auto bg-[rgba(242,250,254,0.8)] mx-auto p-[clamp(1rem,1vw,2.5rem)] flex flex-col gap-2 tablet:w-1/2 tablet:my-auto tablet:top-[20%] tablet:bottom-[20%] tablet:right-auto ">
+        <Header preset="h6">Grand Launching Depok Kita UI 2022</Header>
+        <Body preset="p3">
+          Pembukaan kegiatan pengabdian masyarakat Departemen Sosial Masyarakat
+          BEM UI 2022
+        </Body>
+        <div className="mt-auto">
+          <Button
+            preset="secondaryLight"
+            rightIcon={<ChevronRightIcon className="w-4" />}
+            className="grow-0"
+          >
+            Grand Opening
+          </Button>
+        </div>
+      </div>
     </div>
   );
 };
