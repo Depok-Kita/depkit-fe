936d3d1d1ec3ea8370651e7b6beb291836477e6d
edutjie <edutjitrahardja@gmail.com>
2022-07-26 17:17:28 +0700

    Basic function of the form done

--- components/modules/grand-opening/GrandOpeningForm.tsx
+++ components/modules/grand-opening/GrandOpeningForm.tsx
@@ -1,14 +1,87 @@
-import React from "react";
+import React, { useRef, useState, useEffect } from "react";
 import axios from "axios";
-import { Header, DonationCard, Card, Body, Button } from "@components";
+import { Header, DonationCard, Card, Body, Button, Toast } from "@components";
 import { InputField } from "@components/input-field";
 import { Divider } from "@chakra-ui/react";
 import { ChevronRightIcon } from "@heroicons/react/outline";
+import { useWindowSize } from "@hooks";
+import Image from "next/image";
 
 export const GrandOpeningForm = () => {
+  const donationProveUpload = useRef<any>(null);
+  const [image, setImage] = useState(null);
+  const [createObjectURL, setCreateObjectURL] = useState(null);
+  const { width } = useWindowSize();
+  const [isLoading, setIsLoading] = useState(false);
+  const successToast = Toast({
+    preset: "success",
+    message: "Anda Berhasil Mendaftar!",
+  });
+  const errorToast = Toast({
+    preset: "error",
+    message: "Gagal Melakukan Pendaftaran!",
+  });
+  const imageErrorToast = Toast({
+    preset: "error",
+    message: "Lampiran Gambar Gagal Diunggah!",
+  });
+
+  const uploadToClient = (event: any) => {
+    if (event.target.files && event.target.files[0]) {
+      const i = event.target.files[0];
+      setImage(i);
+      setCreateObjectURL(URL.createObjectURL(i) as any);
+    }
+  };
+
+  const submitImageHandler = async (image: any) => {
+    const formData = new FormData();
+    formData.append("files", image);
+    try {
+      const response = await axios.post(
+        `${process.env.NEXT_PUBLIC_API_STRAPI}api/upload/`,
+        formData
+      );
+      return response.data[0].id;
+    } catch (err) {
+      imageErrorToast();
+    }
+  };
+
+  const handleSubmit = async (e: any) => {
+    e.preventDefault();
+    setIsLoading(true);
+    const imageId = await submitImageHandler(image);
+    const data = {
+      name: e.target.name.value,
+      email: e.target.email.value,
+      line: e.target.line.value,
+      donationProve: imageId,
+      phone: e.target.phone.value,
+    };
+    console.log(data);
+    await axios
+      .post("/api/grand-opening", {
+        data: data,
+      })
+      .then((response) => {
+        console.log(response);
+        successToast();
+      })
+      .catch((err) => {
+        console.log(err.response.data.message);
+        errorToast();
+      });
+    setIsLoading(false);
+  };
+
   return (
     <div className="bg-denim-light py-28">
-      <form action="" method="post" className="space-y-10 max-w-[70%] mx-auto">
+      <form
+        encType="multipart/form-data"
+        className="space-y-10 max-w-[70%] mx-auto"
+        onSubmit={handleSubmit}
+      >
         <div className="grid grid-cols-8 gap-10">
           <div className="w-full flex flex-col gap-[20px] col-span-5">
             <div>
@@ -24,6 +97,7 @@ export const GrandOpeningForm = () => {
           <div className="flex flex-col gap-10 justify-end mb-8 w-full text-totalwhite col-span-3">
             <InputField
               type="text"
+              name="name"
               placeholder="Masukkan Nama Lengkap Anda"
               label="Nama Lengkap"
               isDisabled={false}
@@ -35,6 +109,7 @@ export const GrandOpeningForm = () => {
             />
             <InputField
               type="text"
+              name="email"
               placeholder="Masukkan Email Anda"
               label="Email"
               isDisabled={false}
@@ -46,6 +121,7 @@ export const GrandOpeningForm = () => {
             />
             <InputField
               type="text"
+              name="phone"
               placeholder="Masukkan Nomor WhatsApp Anda"
               label="Nomor WhatsApp"
               isDisabled={false}
@@ -57,6 +133,7 @@ export const GrandOpeningForm = () => {
             />
             <InputField
               type="text"
+              name="line"
               placeholder="Masukkan ID Line Anda"
               label="ID Line"
               isDisabled={false}
@@ -68,15 +145,16 @@ export const GrandOpeningForm = () => {
           </div>
         </div>
         <Card preset="dark">
-          <div className="flex flex-col gap-10">
-            <div className="space-y-3">
+          <div className="flex flex-col gap-10 items-center w-full">
+            <div className="flex flex-col gap-3">
               <Header preset="h4" className="text-powder-light">
                 Kirim bukti donasi
               </Header>
               <Divider
-                className=""
-                backgroundColor="#88BFE8"
+                borderColor={"#88BFE8"}
+                bgColor={"#88BFE8"}
                 borderWidth={1.5}
+                width={width > 768 ? width / 1.6 : "auto"}
               />
               <Body
                 preset="p3"
@@ -87,16 +165,47 @@ export const GrandOpeningForm = () => {
             </div>
             <div className="mr-auto">
               <Button
+                type="button"
                 preset="secondaryDark"
                 className="w-full"
                 rightIcon={<ChevronRightIcon className="w-5" />}
+                onClick={() => {
+                  donationProveUpload.current.click();
+                }}
               >
-                Pilih Foto
+                {!!createObjectURL ? "Ubah Foto" : "Pilih Foto"}
               </Button>
+              <input
+                hidden
+                required
+                type="file"
+                name="donationProve"
+                id="donationProve"
+                accept="image/*"
+                ref={donationProveUpload}
+                onChange={uploadToClient}
+              />
             </div>
+            {createObjectURL && (
+              <div className="w-[50%]">
+                <Image
+                  src={createObjectURL}
+                  alt="Open Donation"
+                  className="rounded-3xl "
+                  layout="responsive"
+                  objectFit="cover"
+                  width={35}
+                  height={35}
+                />
+              </div>
+            )}
           </div>
         </Card>
-        <Button preset="primaryLight" className="mx-auto scale-125">
+        <Button
+          preset="primaryLight"
+          className="mx-auto scale-125"
+          disabled={isLoading}
+        >
           Daftar
         </Button>
       </form>
---
+++ pages/api/grand-opening/index.ts
@@ -0,0 +1,30 @@
+import type { NextApiRequest, NextApiResponse } from "next";
+import axios from "axios";
+
+export default function handler(req: NextApiRequest, res: NextApiResponse) {
+  if (req.method !== "POST") {
+    res.status(403).end("Must use POST method!");
+  }
+
+  if (!req.body) {
+    res.status(400).end("Must provide request body!");
+  }
+
+  const { data } = req.body;
+
+  axios
+    .post(`${process.env.NEXT_PUBLIC_API_STRAPI}api/grand-openings`, {
+      data: data,
+    })
+    .then((response) => res.status(200).send(response.data))
+    .catch((e) => {
+      console.log(e.response.data.error);
+      res.status(500).send(e.response.data.error);
+    });
+}
+
+export const config = {
+  api: {
+    externalResolver: true,
+  },
+};
