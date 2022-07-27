2b0b1e5a956dedd2357f78a5e01b61da4204c342
Naufal Weise <naufalwwidyatama@gmail.com>
2022-07-27 12:41:51 +0700

    Use original image

--- constants/home-carousel-images.ts
+++ constants/home-carousel-images.ts
@@ -1,5 +1,5 @@
 export const HOME_CAROUSEL_IMAGES: string[] = [
   "/assets/images/home-img.png",
-  "/assets/images/home-img2.png",
-  "/assets/images/home-img3.png",
+  "/assets/images/home-img.png",
+  "/assets/images/home-img.png",
 ];
