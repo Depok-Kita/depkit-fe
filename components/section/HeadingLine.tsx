7bc0238b0f2f4b9674a6444f9fab899aa3a46269
Naufal Weise <naufalwwidyatama@gmail.com>
2022-07-27 10:54:30 +0700

    Add heading line

---
+++ components/section/HeadingLine.tsx
@@ -0,0 +1,8 @@
+// add mx-auto to center the line
+export const HeadingLine = ({ className }: { className?: string }) => {
+  return (
+    <div
+      className={"h-[4.5px] w-[80px] tablet:w-[220px] bg-cerulean " + className}
+    ></div>
+  );
+};
