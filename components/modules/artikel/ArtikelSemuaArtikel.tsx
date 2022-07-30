3e990861f72ce68a72d119a5722ee2335b49b525
eugeniusms <eugeniusms@gmail.com>
2022-07-30 07:44:14 +0700

    ArtikelSemuaArtikel resp : tablet

--- components/modules/artikel/ArtikelSemuaArtikel.tsx
+++ components/modules/artikel/ArtikelSemuaArtikel.tsx
@@ -3,6 +3,7 @@ import { Body, Button, InputField } from "@components";
 import { SearchIcon } from "@heroicons/react/outline";
 import { ArtikelArtikelLainnya } from "@components";
 import { useState } from "react";
+import { useWindowSize } from "@hooks";
 
 type ArtikelSemuaArtikelProps = {
   className?: string;
@@ -17,6 +18,7 @@ export const ArtikelSemuaArtikel = ({
   articles,
   topics,
 }: ArtikelSemuaArtikelProps) => {
+  const { width } = useWindowSize();
   // Default value total (total artikel yang ditampilkan) = 5
   const [totalArticles, setTotalArticles] = useState(5);
   // Default value selectedTopic = All
@@ -41,9 +43,13 @@ export const ArtikelSemuaArtikel = ({
   return (
     <div className="flex justify-center bg-powder-light">
       <div className="flex flex-col w-full" id="search-section">
-        <div className="flex gap-2 items-center mobile:px-7 mobile:pt-4">
-          <SmallBlueLine />
-          <Body preset="b2">Semua Artikel</Body>
+        <div className="flex gap-2 items-center mobile:px-7 mobile:pt-4 tablet:pl-16 tablet:pt-10 tablet:pb-5">
+          <Body
+            preset="b2"
+            className="border-l-2 border-cerulean px-2 tablet:border-l-4 tablet:text-[24px] tablet:px-4"
+          >
+            Semua Artikel
+          </Body>
         </div>
         <div className="flex justify-end pt-5 pb-3">
           <input
@@ -54,18 +60,18 @@ export const ArtikelSemuaArtikel = ({
           />
         </div>
         {/* BAGIAN TOPIC */}
-        <div className="pl-8 py-3">
-          <Body preset="p3" className="pb-2">
+        <div className="pl-8 py-3 tablet:pl-20">
+          <Body preset={width >= 768 ? "p2" : "p3"} className="pb-2">
             Cari artikel berdasarkan topik
           </Body>
           {/* KUMPULAN TOPIC */}
-          <div className="flex flex-wrap mobile:max-w-[240px] gap-1">
+          <div className="flex flex-wrap mobile:max-w-[240px] gap-1 tablet:max-w-[440px]">
             {/* TOPIC ALL (DEFAULT) */}
             <div
               className={
                 selectedTopic == "All"
-                  ? "border-2 bg-cerulean border-cerulean rounded-2xl text-powder-light text-[8px] font-bold px-[6px] py-[1px]"
-                  : "border-2 border-cerulean rounded-2xl text-cerulean text-[8px] font-bold px-[6px] py-[1px]"
+                  ? "border-2 bg-cerulean border-cerulean rounded-2xl text-powder-light text-[8px] font-bold px-[6px] py-[1px] tablet:text-[11px]"
+                  : "border-2 border-cerulean rounded-2xl text-cerulean text-[8px] font-bold px-[6px] py-[1px] tablet:text-[11px]"
               }
               key={0}
               onClick={changeSelectedTopicHandler}
@@ -78,8 +84,8 @@ export const ArtikelSemuaArtikel = ({
               <div
                 className={
                   selectedTopic == topic.name
-                    ? "border-2 bg-cerulean border-cerulean rounded-2xl text-powder-light text-[8px] font-bold px-[6px] py-[1px]"
-                    : "border-2 border-cerulean rounded-2xl text-cerulean text-[8px] font-bold px-[6px] py-[1px]"
+                    ? "border-2 bg-cerulean border-cerulean rounded-2xl text-powder-light text-[8px] font-bold px-[6px] py-[1px] tablet:text-[11px]"
+                    : "border-2 border-cerulean rounded-2xl text-cerulean text-[8px] font-bold px-[6px] py-[1px] tablet:text-[11px]"
                 }
                 key={topic.id}
                 onClick={changeSelectedTopicHandler}
@@ -96,7 +102,7 @@ export const ArtikelSemuaArtikel = ({
           filter={selectedTopic}
           search={enteredSearchInput}
         />
-        <div className="flex justify-center pt-6 pb-10">
+        <div className="flex justify-center pt-6 pb-10 tablet:pt-10 tablet:pb-14">
           <Button
             preset="secondaryLight"
             className="w-[160px]"
