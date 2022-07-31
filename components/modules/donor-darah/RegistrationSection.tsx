cac1ca8c2f79c0be43497e6bcc159a1e48261975
Naufal Weise <naufalwwidyatama@gmail.com>
2022-07-31 19:03:34 +0700

    Add donor darah form validation

--- components/modules/donor-darah/RegistrationSection.tsx
+++ components/modules/donor-darah/RegistrationSection.tsx
@@ -1,7 +1,50 @@
 import { Header, HeadingLine, SectionBox, Body, Button } from "@components";
-import { InputField } from "@components/input-field";
+import { InputField, InputFieldFormik } from "@components/input-field";
+import { Formik, Form, FormikErrors } from "formik";
+import { DonorDarah } from "@models";
+import { isValidPhoneNumber } from "@utils";
+
+const COUNTRY_CODE = "+62";
 
 export const RegistrationSection = () => {
+  const initalValues: DonorDarah = {
+    name: "",
+    age: 0,
+    address: "",
+    institute: "",
+    email: "",
+    phone: "",
+    line: "",
+    donationDate: "",
+  };
+  const MIN_AGE = 17;
+  const REQUIRED_ERROR_MSG = "Harus diisi!";
+  const validate = (values: DonorDarah) => {
+    let errors: FormikErrors<DonorDarah> = {};
+    if (!values.name) {
+      errors.name = REQUIRED_ERROR_MSG;
+    }
+    if (!values.age) {
+      errors.age = REQUIRED_ERROR_MSG;
+    } else if (values.age < MIN_AGE) {
+      errors.age = "Usia minimal " + MIN_AGE;
+    }
+    if (!values.address) {
+      errors.address = REQUIRED_ERROR_MSG;
+    }
+    if (!values.email) {
+      errors.email = REQUIRED_ERROR_MSG;
+    }
+    if (!values.phone) {
+      errors.phone = REQUIRED_ERROR_MSG;
+    } else if (!isValidPhoneNumber(values.phone)) {
+      errors.phone = "Nomor telepon tidak valid. Gunakan format +628XXXXXXXX";
+    }
+    if (!values.donationDate) {
+      errors.donationDate = REQUIRED_ERROR_MSG;
+    }
+    return errors;
+  };
   return (
     <div className="bg-denim-dark ">
       <SectionBox>
@@ -11,42 +54,88 @@ export const RegistrationSection = () => {
           </Header>
           <HeadingLine className="mx-auto" />
         </div>
-        <div className="flex flex-col gap-2 max-w-sm mx-auto">
-          <InputField
-            type="text"
-            label="Nama Lengkap"
-            placeholder="Nama Lengkap"
-            dark
-            required
-          />
-          <InputField
-            type="number"
-            label="Usia"
-            placeholder="Masukkan angka"
-            dark
-            required
-          />
-          <InputField type="text" label="Alamat" placeholder="" dark required />
-          <InputField type="text" label="Asal Instansi" dark />
-          <InputField
-            type="email"
-            label="Email"
-            placeholder="JohnDoe@gmail.com"
-            dark
-            required
-          />
-          <InputField
-            type="number"
-            label="Nomor Telepon/WhatsApp*"
-            placeholder="+62"
-            dark
-            required
-          />
-          <InputField type="text" label="Id Line" dark />
-          <InputField type="date" label="Tanggal Donor Darah" required />
-          <Button preset="primaryLight" className="mt-2 grow-0 shrink-1">
-            Daftar
-          </Button>
+        <div className="">
+          <Formik
+            initialValues={initalValues}
+            validate={validate}
+            onSubmit={(values, props) => {
+              values.phone = COUNTRY_CODE + values.phone;
+              console.log(values);
+              props.setSubmitting(false);
+            }}
+          >
+            {(props) => (
+              <Form className="flex flex-col gap-2 max-w-sm mx-auto">
+                <InputFieldFormik
+                  name="name"
+                  type="text"
+                  label="Nama Lengkap"
+                  placeholder="Nama Lengkap"
+                  dark
+                  required
+                />
+                <InputFieldFormik
+                  name="age"
+                  type="number"
+                  label="Usia"
+                  placeholder="Masukkan angka"
+                  dark
+                  required
+                />
+                <InputFieldFormik
+                  name="address"
+                  type="text"
+                  label="Alamat"
+                  placeholder=""
+                  dark
+                  required
+                />
+                <InputFieldFormik
+                  name="institute"
+                  type="text"
+                  label="Asal Instansi"
+                  dark
+                />
+                <InputFieldFormik
+                  name="email"
+                  type="email"
+                  label="Email"
+                  placeholder="JohnDoe@gmail.com"
+                  dark
+                  required
+                />
+                <InputFieldFormik
+                  name="phone"
+                  type="tel"
+                  label="Nomor Telepon/WhatsApp*"
+                  placeholder={COUNTRY_CODE}
+                  dark
+                  required
+                />
+                <InputFieldFormik
+                  name="line"
+                  type="text"
+                  label="Id Line"
+                  dark
+                />
+                <InputFieldFormik
+                  name="donationDate"
+                  type="date"
+                  label="Tanggal Donor Darah"
+                  required
+                  dark
+                />
+                <Button
+                  preset="primaryLight"
+                  className="mt-4  self-center"
+                  type="submit"
+                  disabled={props.isSubmitting}
+                >
+                  Daftar
+                </Button>
+              </Form>
+            )}
+          </Formik>
         </div>
       </SectionBox>
     </div>
