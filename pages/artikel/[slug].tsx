91c9b3316958c50b083f43b2853bca60b8b5af51
eugeniusms <eugeniusms@gmail.com>
2022-07-31 18:53:24 +0700

    [slug] resp : tablet

--- pages/artikel/[slug].tsx
+++ pages/artikel/[slug].tsx
@@ -13,8 +13,11 @@ import Link from "next/link";
 import { Article } from "@models";
 import { useRouter } from "next/router";
 import Image from "next/image";
+import { useWindowSize } from "@hooks";
 
 const Details = (artikel: any) => {
+  const { width } = useWindowSize();
+
   const router = useRouter();
   const { slug } = router.query;
 
@@ -44,7 +47,7 @@ const Details = (artikel: any) => {
         <div className="w-5/6">
           {/* NAVBAR SPACE */}
           <div className="flex flex-col">
-            <div className="h-[100px]"></div>
+            <div className="h-[100px] tablet:h-[150px]"></div>
             {/* TOMBOL KEMBALI */}
             <Link href="/artikel/">
               <div className="bg-powder-light border-powder-light drop-shadow-lg rounded-3xl border-2 w-9 hover:cursor-pointer">
@@ -55,10 +58,16 @@ const Details = (artikel: any) => {
             {/* JUDUL DAN TANGGAL RILIS ARTIKEL */}
             <div className="border-l-4 border-cerulean">
               <div className="px-3">
-                <Header preset="h5" className="font-black text-[18px]">
+                <Header
+                  preset="h5"
+                  className="font-black text-[18px] tablet:text-[28px]"
+                >
                   {article?.title}
                 </Header>
-                <Body preset="p3" className="text-[9px] font-semibold">
+                <Body
+                  preset="p3"
+                  className="text-[9px] font-semibold tablet:text-[15px]"
+                >
                   {"diterbitkan pada tanggal " + dateFormat(article?.published)}
                 </Body>
               </div>
@@ -68,18 +77,18 @@ const Details = (artikel: any) => {
               <Image
                 src={article?.photoUrl}
                 alt={article?.photoAlt}
-                width={270}
-                height={180}
-                className="rounded-lg"
+                width={width >= 768 ? 560 : 270}
+                height={width >= 768 ? 340 : 180}
+                className="rounded-lg tablet:rounded-3xl"
               />
             </div>
             {/* BADAN ARTIKEL */}
-            <div className="flex flex-col gap-4 text-[12px]">
+            <div className="flex flex-col gap-4 text-[12px] tablet:text-[16px] tablet:gap-6">
               <ReactMarkdown>{article?.body}</ReactMarkdown>
             </div>
             {/* ULIRAN */}
             <div className="flex justify-center pt-12">
-              <UliranArticles />
+              <UliranArticles className="tablet:w-[600px]" />
             </div>
             {/* ARTIKEL LAINNYA */}
             <div className="pb-14">
