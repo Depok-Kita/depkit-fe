1768baa864a538473669f8314e1007552b9de1fc
Naufal Weise <naufalwwidyatama@gmail.com>
2022-08-01 19:09:03 +0700

    Style tujuan section

--- components/modules/donor-darah/TujuanSection.tsx
+++ components/modules/donor-darah/TujuanSection.tsx
@@ -1,15 +1,38 @@
-import { Header, Card, Body, SectionBox } from "@components";
+import {
+  HeaderResponsive as Header,
+  Card,
+  BodyResponsive as Body,
+  SectionBox,
+  useWindowSize,
+  HeadingLine,
+} from "@components";
+
+import Decor from "@images/donor-darah-decor-2.svg";
 
 export const TujuanSection = () => {
+  const windowSize = useWindowSize();
   return (
     <div className="relative bg-powder">
-      <SectionBox>
-        <Header preset="h3" className="text-center mb-2">
-          Tujuan
-        </Header>
+      <Decor className="hidden desktop:block absolute top-0 left-0 " />
+      <Decor className="hidden desktop:block absolute bottom-0 right-0 -scale-x-100" />
+      <SectionBox className="desktop:pt-20 desktop:pb-36">
+        <div className="mb-8 tablet:mb-10">
+          <Header
+            windowSize={windowSize}
+            preset="h5"
+            presetTablet="h3"
+            presetDesktop="h2"
+            className="text-center"
+          >
+            Tujuan
+          </Header>
+          <HeadingLine className="mx-auto" />
+        </div>
         <Card preset="light">
-          <Header preset="h5">Lorem ipsum dolor sit amet</Header>
-          <Body preset="p3">
+          <Header windowSize={windowSize} preset="h6" presetTablet="h5">
+            Lorem ipsum dolor sit amet
+          </Header>
+          <Body windowSize={windowSize} preset="p3" presetTablet="p2">
             Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam
             consequat risus eu mi egestas, non rutrum mauris laoreet. Ut eget
             dignissim metus, non vehicula dolor. Praesent at magna vel velit
