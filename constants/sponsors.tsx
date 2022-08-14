5dfc03d9f43d68fc3a1dcbac97f6eda3f34ac17a
marcellinuselbert11 <marcellinuselbert46@gmail.com>
2022-08-14 19:24:03 +0700

    Update sponsors, deactivate medpars, and fix hydration error

---
+++ constants/sponsors.tsx
@@ -0,0 +1,14 @@
+import LogoBulog from "@images/logo-bulog.svg";
+import LogoInsight from "@images/logo-insight.svg";
+import { Partnership } from "@models";
+
+export const SPONSORS: Partnership[] = [
+  {
+    size: "small",
+    logos: [LogoInsight],
+  },
+  {
+    size: "large",
+    logos: [LogoBulog],
+  },
+];
