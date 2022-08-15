1a770edceee5a1952cdd67ee4755a041a86175c8
edutjie <edutjitrahardja@gmail.com>
2022-08-15 16:19:29 +0700

    Added meta image

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,11 +3,17 @@ import theme from "@styles/theme";
 import type { AppProps } from "next/app";
 import { ChakraProvider } from "@chakra-ui/react";
 import { Navbar, Footer } from "@components";
+import Head from "next/head";
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <ChakraProvider theme={theme}>
       <Navbar />
+      <Head>
+        <title>Depok Kita</title>
+        <link rel="icon" href="/assets/images/depkit-logo-tabs.svg" />
+        <meta property="og:image" content="/assets/images/meta.png" />
+      </Head>
       <Component {...pageProps} />
       <Footer />
     </ChakraProvider>
