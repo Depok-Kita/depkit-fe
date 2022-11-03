ef2bf34dc99bc7bd73a30f6f8fc432bb7420f2de
marcellinuselbert <elbertmarcellinus@gmail.com>
2022-11-03 10:08:45 +0700

    change sop on donor darah

--- components/modules/donor-darah/SopSection.tsx
+++ components/modules/donor-darah/SopSection.tsx
@@ -67,8 +67,8 @@ export const SopSection = () => {
             <b>minimal 60 hari</b>
           </li>
           <li>
-            <b>Bersedia hadir</b> di lokasi donor darah, yaitu Masjid
-            Baiturrahman Kp Lio, Depok
+            <b>Bersedia hadir</b> PMI Depok dengan akomodasi transport dari
+            panitia Depok Kita
           </li>
         </ol>
       </div>
